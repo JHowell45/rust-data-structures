c2752c977f78529881808e98039ddbd784394f65
Jacob Howell <jhowell45@googlemail.com>
2022-03-07 21:24:33 +0000

    Creating linked list struct and data types.

---
+++ linked_list/target/release/deps/linked_list-0a30afc71ef6c36f.d
@@ -0,0 +1,5 @@
+C:\Users\jhowe\Documents\Code\rust-data-structures\linked_list\target\release\deps\linked_list-0a30afc71ef6c36f.exe: src\lib.rs
+
+C:\Users\jhowe\Documents\Code\rust-data-structures\linked_list\target\release\deps\linked_list-0a30afc71ef6c36f.d: src\lib.rs
+
+src\lib.rs:
---
+++ linked_list/target/release/deps/linked_list-24476dd7ca4a214f.d
@@ -0,0 +1,7 @@
+C:\Users\jhowe\Documents\Code\rust-data-structures\linked_list\target\release\deps\linked_list-24476dd7ca4a214f.rmeta: src\lib.rs
+
+C:\Users\jhowe\Documents\Code\rust-data-structures\linked_list\target\release\deps\liblinked_list-24476dd7ca4a214f.rlib: src\lib.rs
+
+C:\Users\jhowe\Documents\Code\rust-data-structures\linked_list\target\release\deps\linked_list-24476dd7ca4a214f.d: src\lib.rs
+
+src\lib.rs:
