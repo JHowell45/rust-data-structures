73111bf09a2a457dfdf60b683b5112c66c378e5c
Jacob Howell <jhowell45@googlemail.com>
2022-03-08 22:51:53 +0000

    Created get_length function and updated get_child to be immutable and added a mutable version of the method.

--- linked_list/src/single_linked_list.rs
+++ linked_list/src/single_linked_list.rs
@@ -16,6 +16,10 @@ impl<T> SingleLinkedList<T> where T: Copy {
         self.child = Some(child);
     }
 
+    pub fn add_child_by_value(&mut self, payload: T) {
+        self.child = Some(Box::new(SingleLinkedList::new(payload, None)));
+    }
+
     pub fn set_value(&mut self, value: T) {
         self.payload = value;
     }
@@ -24,7 +28,34 @@ impl<T> SingleLinkedList<T> where T: Copy {
         return self.payload.clone();
     }
 
-    pub fn get_child(&mut self) -> Option<&mut Box<SingleLinkedList<T>>> {
+    pub fn get_child(&self) -> Option<&Box<SingleLinkedList<T>>> {
+        return self.child.as_ref();
+    }
+
+    pub fn get_mut_child(&mut self) -> Option<&mut Box<SingleLinkedList<T>>> {
         return self.child.as_mut();
     }
+
+    pub fn has_child(&self) -> bool {
+        return self.child.is_some();
+    }
+
+    pub fn get_length(&self) -> u32 {
+        let mut count: u32 = 0;
+        if self.has_child() {
+            count += self.get_child().unwrap().get_length();
+        }
+        count += 1;
+        return count;
+    }
+
+    // pub fn get_node_by_value(&self, value: T) -> Option<&mut Box<SingleLinkedList<T>>> {
+    //     if self.get_value() == value {
+    //         panic!("fuck");
+    //     }
+    //     while self.has_child() {
+
+    //     }
+    //     return None;
+    // }
 }
--- linked_list/tests/single_linked_list/mod.rs
+++ linked_list/tests/single_linked_list/mod.rs
@@ -34,12 +34,62 @@ fn get_child() {
 }
 
 #[test]
+fn has_child() {
+    let child: SingleLinkedList<i32> = SingleLinkedList::new(2, None);
+    let child_box = Box::new(child);
+    let mut link: SingleLinkedList<i32> = SingleLinkedList::new(1, None);
+    assert_eq!(link.has_child(), false);
+    link.add_child(child_box);
+    assert_eq!(link.has_child(), true);
+}
+
+#[test]
 fn update_child_data() {
     let child: SingleLinkedList<i32> = SingleLinkedList::new(2, None);
     let child_box = Box::new(child);
     let mut link: SingleLinkedList<i32> = SingleLinkedList::new(1, Some(child_box.clone()));
-    let retrieved_child = link.get_child().unwrap();
+    let retrieved_child = link.get_mut_child().unwrap();
     retrieved_child.set_value(10);
     assert_eq!(retrieved_child.get_value(), 10);
     assert_eq!(link.get_child().unwrap().get_value(), 10);
+}
+
+#[test]
+fn get_length_test_1() {
+    let mut link = SingleLinkedList::new(10, None);
+    assert_eq!(link.get_length(), 1);
+}
+
+#[test]
+fn get_length_test_2() {
+    let mut link = SingleLinkedList::new(10, None);
+    link.add_child_by_value(1);
+    assert_eq!(link.get_length(), 2);
+}
+
+#[test]
+fn get_length_test_3() {
+    let mut link = SingleLinkedList::new(10, None);
+    link.add_child_by_value(1);
+    link.get_mut_child().unwrap().add_child_by_value(2);
+    assert_eq!(link.get_length(), 3);
+}
+
+#[test]
+fn get_length_test_4() {
+    let mut link = SingleLinkedList::new(10, None);
+    link.add_child_by_value(1);
+    link.get_mut_child().unwrap().add_child_by_value(2);
+    link.get_mut_child().unwrap().get_mut_child().unwrap().add_child_by_value(2);
+    assert_eq!(link.get_length(), 4);
+}
+
+#[test]
+fn get_length_test_5() {
+    let mut link = SingleLinkedList::new(10, None);
+    link.add_child_by_value(1);
+    link.get_mut_child().unwrap().add_child_by_value(2);
+    link.get_mut_child().unwrap().get_mut_child().unwrap().add_child_by_value(2);
+    link.get_mut_child().unwrap().get_mut_child().unwrap().get_mut_child().unwrap().add_child_by_value(2);
+    assert_eq!(link.get_length(), 5);
 }
